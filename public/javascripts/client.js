9d7d39edb2816f74826db9831d06ab094887b26f
Rémi Bouchar <remi.bouchar@gmail.com>
2012-12-25 16:41:25 +0100

    login, pass and path are done, next: add local storage

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -5,33 +5,40 @@ var decrypted = CryptoJS.AES.decrypt(s, "Secret Passphrase");
 r=CryptoJS.enc.Utf8.stringify(decrypted);
 //console.log(r);
 
-var pn = pn?pn:function($,CryptoJS){
+
+/**
+* login : the user login
+  pass : the user pass, used for aes encryption
+  path : the path of the file
+  key : the sha1 of login+pass, used as an index in the local store
+*/
+var pn = function($,CryptoJS){
 Store = [];
 if (!$ || !CryptoJS){
 	console.error("Missing Dependencies");
 }
 
 	this.init = function(){
-		$("#key").change(function(){
+		$("#login,#pass,#path").change(function(){
 		load()
 		});
 		$("#input").change(function(){
-			
 			save();
 		});
 	}
-
+//TODO use login+key as sha1 to avoid transmission of the encrypted key and thirdpart overidding
 function load(){
 	console.log("loading");
 	$("#input").val("");
-	var key=$("#key").val();
-	var ekey=CryptoJS.SHA1(key).toString();
 	var login=$("#login").val();
-	if(Store[login]&&Store[login][ekey]){
-	var source=Store[login][ekey];
+	var pass=$("#pass").val();
+	var path=$("#path").val();
+	var key=CryptoJS.SHA1(login+pass).toString();
+	if(Store[key]&&Store[key][path]){
+	var source=Store[key][path];
 }
 	if(source){
-		var raw=CryptoJS.AES.decrypt(source, $("#key").val());
+		var raw=CryptoJS.AES.decrypt(source,pass);
 		var result=CryptoJS.enc.Utf8.stringify(raw);
 		$("#input").val(result.toString());
 		console.log("Loaded");
@@ -41,14 +48,15 @@ function load(){
 function save(){
 	console.log("saving")
 	var input=$("#input").val();
-	var key=$("#key").val();
 	var login=$("#login").val();
-	var result=CryptoJS.AES.encrypt(input,key).toString();
-	var sha=CryptoJS.SHA1(key).toString();
-	if(!Store[login]){
-		Store[login]=[];
+	var pass=$("#pass").val();
+	var path=$("#path").val();
+	var result=CryptoJS.AES.encrypt(input,pass).toString();
+	var key=CryptoJS.SHA1(login+pass).toString();
+	if(!Store[key]){
+		Store[key]=[];
 	}
-	Store[login][sha]=result
+	Store[key][path]=result
 	console.log("Saved");
 	console.log(Store);
 }
