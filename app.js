fa973d6060e435949d1d359bb138497555dda607
Rémi Bouchar <remi.bouchar@gmail.com>
2013-01-02 18:52:36 +0100

    Modified the loggin level of the nodeserver

--- app.js
+++ app.js
@@ -16,11 +16,12 @@ app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.logger("short"));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  
 });
 
 app.configure('development', function() {
