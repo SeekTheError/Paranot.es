d5ddd010077dd859fdb0a35fa29db05fefc31bf5
Rémi Bouchar <remi.bouchar@gmail.com>
2013-01-07 23:47:03 +0100

    add socket.io + redis store base ; use the cluster module

--- app.js
+++ app.js
@@ -2,42 +2,111 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-  routes = require('./routes'),
-  http = require('http'),
-  path = require('path'); 
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
-var app = express();
+//min number on cpu  
+numCPUs == 1 ? numCPUs = 2 : '';
 
-port=process.argv[2];
+// code for handling worker, one by cpu unit
+if(cluster.isMaster) {
+  var timeouts = [];
 
-app.configure(function() {
-  app.set('port', port || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.logger("short"));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  
-});
+  function errorMsg(worker) {
+    console.error("Something must be wrong with the connection ... " + (worker ? worker.id : ''));
+    console.log()
 
-app.configure('development', function() {
-  app.use(express.errorHandler());
-});
+  }
+  //when a new worker is connected
+  cluster.on('online', function(worker) {
+    console.log("new worker online: " + worker.id);
+  });
+  // when a worker is forked
+  cluster.on('fork', function(worker) {
+    timeouts[worker.id] = setTimeout(errorMsg, 2000);
+  });
+  //when the master proccess check a worker 
+  cluster.on('listening', function(worker, address) {
+    clearTimeout(timeouts[worker.id]);
+  });
+  //when a worker is terminated
+  cluster.on('exit', function(worker, code, signal) {
+    clearTimeout(timeouts[worker.id]);
+    errorMsg(worker);
+    console.log("trying to restart a worker");
+    cluster.fork();
+  });
 
-app.get('/', routes.index);
-app.post('/save', routes.save);
-app.post('/load', routes.load);
-app.post('/createUser', routes.createUser);
-app.post('/checkUser', routes.checkUser);
-app.post('/deleteFile', routes.deleteFile);
+  console.log("forking " + numCPUs + "workers")
+  // Fork workers.
+  for(var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
 
-http.createServer(app).listen(app.get('port'), function() {
-  console.log("Express server listening on port " + app.get('port'));
-});
+}
+// code for the forks
+else {
+  var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+    socketio = require('socket.io');
+
+  var app = express();
+
+  port = process.argv[2];
+
+  app.configure(function() {
+    app.set('port', port || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.logger("short"));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+
+  });
+
+  app.configure('development', function() {
+    app.use(express.errorHandler());
+  });
+
+  app.get('/', routes.index);
+  app.post('/save', routes.save);
+  app.post('/load', routes.load);
+  app.post('/createUser', routes.createUser);
+  app.post('/checkUser', routes.checkUser);
+  app.post('/deleteFile', routes.deleteFile);
 
 
+  var server=http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+  });
+
+  io = socketio.listen(server);
+
+  var redisPort = 6379;
+  var redisHost = 'localhost';
+
+  var RedisStore = require('socket.io/lib/stores/redis'),
+    redis = require('socket.io/node_modules/redis'),
+    pub = redis.createClient(redisPort, redisHost),
+    sub = redis.createClient(redisPort, redisHost),
+    client = redis.createClient(redisPort, redisHost);
+
+  /*pub.auth(password, function (err) { if (err) throw err; });
+sub.auth(password, function (err) { if (err) throw err; });
+client.auth(password, function (err) { if (err) throw err; });*/
+
+  io.set('store', new RedisStore({
+    redis: redis,
+    redisPub: pub,
+    redisSub: sub,
+    redisClient: client
+  }));
+
+  require("./socketio/rt.js");
+}
