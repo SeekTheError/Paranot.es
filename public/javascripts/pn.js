c55b6565e84f28acc4d357c4ce73499c50e6f27d
Rémi Bouchar <remi.bouchar@gmail.com>
2013-01-21 02:27:38 +0100

    started to migrate UI related function to ui.js, as well as refactoring the code

--- public/javascripts/pn.js
+++ public/javascripts/pn.js
@@ -9,6 +9,10 @@ PN = function() {
 	if(!$ || !CryptoJS || !Socket) {
 		console.error("Missing Dependencies");
 	}
+
+
+	var pn = this;
+
 	// a way to scope some variables at the pn object level;
 	var Store = {};
 
@@ -16,194 +20,15 @@ PN = function() {
 	 *	on file Creation to autodisplay the new file
 	 *	on reload to display the reloaded file
 	 */
-	Store.nextPath = null;
+	pn.nextPath = null;
 
+	this.isInitialized = false;
 
-	/*
-	 * Event originated from the user interface
-	 */
-
-	var pn=this;
-
-	this.isInitialized =false,
-
+	var ui;
 	(function(pn) {
-		$("#connect").click(function(event) {
-			event.preventDefault();
-			pn.checkUser();
-			return false;
-		});
-		$("#login").keypress(function(event) {
-			if(!(event.which == 13)) {
-				return true;
-			} else {
-				$("#pass").focus();
-				event.preventDefault();
-				return false;
-			}
-		});
-
-		//trigger the user verification when the enter key is pressed
-		$("#pass").keypress(function(event) {
-			if(!(event.which == 13)) {
-				return true;
-			} else {
-				event.preventDefault();
-				pn.checkUser();
-				return false;
-			}
-		});
-
-		// there is no cookie and no form, so reloading the page will log out
-		$("#logout").click(function() {
-			window.location = ""
-		});
-
-		//Reload the current file
-		$("#refresh").click(function(event) {
-			event.preventDefault();
-			document.getElementById("input").innerHTML = "";
-			Store.nextPath = $("#input").data("path");
-			pn.checkUser();
-			return false;
-		})
-		/**
-		 *Load a file when the user click on a tab
-		 *The file to load is determined via the active element bootstrap put on tabs
-		 */
-		$("#inputs-navs li a.file").live('click', function(event) {
-			event.preventDefault();
-			pn.load();
-			return false;
-		})
-		/**
-		 *
-		 */
-		$('#deleteFile').live("click", function(event) {
-			event.preventDefault();
-			var fileName = $("#input").data('path');
-			if(!fileName) {
-				window.alert("Please select the file that you want to delete first");
-				return;
-			}
-			var toDelete = window.confirm("Are you sure that you want to delete the file " + decodeURIComponent(fileName));
-			if(toDelete) {
-				pn.deleteFile();
-			}
-			return false;
-		});
-
-
-		/*
-		 * variable to prevent data loss when changing file
-		 */
-
-		$("#input").focusout(function(event) {
-			event.preventDefault();
-			if(Store.inputSync) {
-				pn.save();
-			}
-		});
-
-		/**
-		 * Save button for tactile interfaces
-		 */
-		$('#save').click(function(event) {
-			event.preventDefault();
-			console.log("SAVE");
-			if(Store.inputSync) {
-				pn.save();
-				Store.inputSync = false;
-			}
-			return false;
-		});
-
-		/**
-		 * empty the new file box on click
-		 */
-		$('#newFileName').click(function(event) {
-			event.preventDefault();
-			$('#newFileName').html("").focus();
-			return false;
-		});
-
-
-		/**
-		 * Auto save when the content is modified
-		 */
-
-		//the time the user has to stop typing for the saved to be performed
-		var TIME_OUT_VALUE = 300;
-		//the saveStatus var is used in the state machine
-		Store.saveStatus = "DONE_TYPING";
-		$("#input").keyup(function(e) {
-			Store.inputContent = $("#input").val();
-			if(Store.saveStatus == "DONE_TYPING") {
-				Store.saveStatus = "IS_TYPING";
-				$("#saveStatus").html("Saving");
-			};
-			if(Store.saveStatus == "IS_TYPING") {
-				//this variable will show weither the content has been modified after the call to
-				//setTimeout. If not it will be saved. Without this, every keyUp event will trigger a save
-				var lastInputContent = $("#input").html();
-				setTimeout(function() {
-					var inputContent = $("#input").html();
-					if(inputContent == lastInputContent && Store.lastSavedInput != inputContent) {
-						Store.saveStatus = "DONE_TYPING";
-						Store.lastSavedInput = inputContent;
-						pn.save();
-
-					} else {
-						return;
-					}
-				}, TIME_OUT_VALUE);
-			}
-		});
-
-
-		/*
-		 *handle ctrl+s to save the #input
-		 */
-		$("#input").live('keypress', function(event) {
-			if(!(event.which == 115 && event.ctrlKey) && !(event.which == 19)) {
-				return true;
-			} else {
-				pn.save();
-				event.preventDefault();
-				return false;
-			}
-		});
-
-		/*
-		 * Create a file when enter is pressed
-		 */
-		$("#newFileName").live('keypress', function(event) {
-			if(!(event.which == 13)) {
-				return true;
-			} else {
-				pn.createFile($('#newFileName').html());
-				event.preventDefault();
-				return false;
-			}
-		});
-		/*
-		 * Either create a new file if the #newFileName has been modified by the user,
-		 * or set the focus on the #newFile
-		 */
-		$("#addNewFile").click(function(event) {
-			event.preventDefault();
-			if($('#newFileName i').length > 0) {
-				$('#newFileName').html("");
-				$('#newFileName').focus();
-			} else {
-				pn.createFile($('#newFileName').html());
-			}
-			return false;
-		});
-		
-		//});
-
-		loadExtensions(pn);pn.isInitialized =true;
+		ui = new UI(pn);
+		loadExtensions(pn);
+		pn.isInitialized = true;
 		console.log("pn is ready");
 	})(this);
 
@@ -215,7 +40,7 @@ PN = function() {
 	 */
 
 	this.load = function(path) {
-		$("#input").hide();
+		ui.hideInput();
 		var login = $("#login").val();
 		var pass = $("#pass").val();
 		//The path if a tab is "clicked"
@@ -236,7 +61,6 @@ PN = function() {
 		Socket.emit("loadFile", data);
 	}
 
-
 	/*
 	 * Method to save the current note on the server
 	 * exposed
@@ -249,32 +73,32 @@ PN = function() {
 		console.log("saving:", path);
 		//TODO check if this bug is gone!!!
 		if(!path) {
+			throw "The save path is not defined"
 			console.log("abort, cause: no path");
 			return;
 		}
 		var content = CryptoJS.AES.encrypt(input, pass).toString();
 		var key = CryptoJS.SHA1(login + pass).toString();
 
-		var url = "/save";
 		var data = {
 			login: encodeURIComponent(login),
 			key: encodeURIComponent(key),
 			path: path,
 			content: content
 		}
-		$("#saveStatus").html("Saving");
+		ui.setSaveStatus(ui.SAVING);
 		Socket.emit('saveFile', data);
 	}
 
 	this.createFile = function(path) {
 		//setting the path to display
-		Store.nextPath = encodeURIComponent(path);
+		pn.nextPath = encodeURIComponent(path);
 		console.log("creating new file: " + path);
-		$("#newFileName").html("<i>New Note</i>")
-		var input = $("#input").html();
+		ui.clearFileName()
 		var login = $("#login").val();
 		var pass = $("#pass").val();
 		var key = CryptoJS.SHA1(login + pass).toString();
+		var content = "x";
 		var data = {
 			login: encodeURIComponent(login),
 			key: encodeURIComponent(key),
@@ -307,15 +131,14 @@ PN = function() {
 			type: 'POST',
 			data: data
 		}).done(function(data) {
-			dispatch(data);
+			dispatchAjaxResponses(data);
 		});
 	}
 
 	/*
-	 * This method is called on user connection attempt
+	 * This method is called upon user connection attempt
 	 */
-
-	this.checkUser = function() {
+	this.loadUserDatas = function() {
 		console.log("checking user");
 		var login = $("#login").val();
 		var pass = $("#pass").val();
@@ -332,21 +155,20 @@ PN = function() {
 			login: encodeURIComponent(login),
 			key: encodeURIComponent(key)
 		}
-		console.log("checkin user: " + login)
+
 		$.ajax({
 			url: url,
 			type: 'POST',
 			data: data
 		}).done(function(data) {
-			dispatch(data);
+			dispatchAjaxResponses(data);
 		})
 	}
 
-	this.createUser= function() {
+	this.createUser = function() {
 		console.log("creating user");
 		var login = $("#login").val();
 		var pass = $("#pass").val();
-
 		var key = CryptoJS.SHA1(login + pass).toString();
 		var url = "/createUser";
 		var data = {
@@ -359,10 +181,9 @@ PN = function() {
 			type: 'POST',
 			data: data
 		}).done(function(data) {
-			dispatch(data);
+			dispatchAjaxResponses(data);
 		})
 	}
-
 	/*
 	 * Decrypt and display a note
 	 */
@@ -379,11 +200,10 @@ PN = function() {
 			var result = CryptoJS.enc.Utf8.stringify(raw);
 			var input = document.getElementById('input');
 			input.innerHTML = result.toString();
-
 			// init the autosave function for the new note
 			Store.lastSavedInput = result.toString();
 			$("#input").data('path', response.path);
-			pn.currentPath=response.path;
+			pn.currentPath = response.path;
 			console.log("Loaded");
 			//this prevent a reload when the .focus method is called
 			Store.inputSync = true;
@@ -391,51 +211,20 @@ PN = function() {
 		}
 	}
 
-	/*
-	 *load or reload the tabs, and load
-	 */
-
-	this.initUserInterface= function(tabs) {
-		console.log("init User Interface", tabs)
-		$("#inputs-navs").html("");
-		$(".command").show();
-		$("#pass").hide();
-		$("#login").hide()
-		$("#connect").hide();
-		$("#userName").html($("#login").val());
-		$('#userName').show();
-		$("#logout").show();
-		var nav;
-		var tab;
-		var empty = true;
-		for(var i = 0; i < tabs.length; i++) {
-			var nav = $('<li>').html('<a class="file" href="#" data-toggle="tab">' + decodeURIComponent(tabs[i]) + '</a>')
-			//if next path has been set on the createFile function
-			if(Store.nextPath == decodeURIComponent(tabs[i])) {
-				$(nav).addClass("active");
-				empty = false;
-			}
-			$("#inputs-navs").append(nav);
-		};
-		//Autoload 
-		if(!empty) {
-			pn.load();
-		}
-	}
 
 	/*
 	 *Route Event originated from a server response
 	 */
 
-	function dispatch(response) {
+	function dispatchAjaxResponses(response) {
 		if(response.status == "userCreated") {
 			console.log("userCreated");
-			pn.checkUser();
+			pn.loadUserDatas();
 			return;
 		}
 		if(response.status == "userExist") {
 			console.log("User Exist");
-			pn.initUserInterface(response.paths);
+			ui.initUserInterface(response.paths);
 			return;
 		}
 		if(response.status == "userDontExist") {
@@ -454,7 +243,7 @@ PN = function() {
 
 		if(response.status == "fileDeleted") {
 			$("#input").data('path', null);
-			pn.checkUser();
+			pn.loadUserDatas();
 			return;
 		}
 		if(response.status == "error") {
@@ -469,23 +258,12 @@ PN = function() {
 
 	Socket.on("userCreated", function() {
 		console.log("userCreated");
-		pn.checkUser();
-	});
-
-	Socket.on("userExist", function() {
-		console.log("User Exist");
-		pn.initUserInterface(response.paths);
-	});
-	Socket.on("userDontExist", function() {
-		var create = confirm("This account does not exist, do you want to create it?");
-		if(create) {
-			createUser();
-		}
+		pn.loadUserDatas();
 	});
 
 	Socket.on("fileCreated", function() {
 		console.log("File successfully created");
-		pn.checkUser();
+		pn.loadUserDatas();
 	});
 	Socket.on("fileAlreadyExist", function() {
 		window.alert("This File name is already taken");
@@ -496,15 +274,15 @@ PN = function() {
 	});
 
 	Socket.on("invalidCredentials", function() {
-		$(".command").hide();
+		ui.hideCommands();
 		window.alert("Invalid Password");
 	});
 	Socket.on("invalidFileName", function() {
 		window.alert("Invalid FileName");
-		$('#newFileName').html('<i>New Note</i>');
+		ui.clearFileName();
 	});
 	Socket.on("fileDeleted", function() {
-		pn.checkUser();
+		pn.loadUserDatas();
 	});
 
 	Socket.on("error", function() {
@@ -512,32 +290,38 @@ PN = function() {
 	});
 
 	Socket.on("fileSaved", function(data) {
-		$("#saveStatus").html("Saved");
+		ui.setSaveStatus(ui.SAVED)
 		console.info("saved");
 	});
 	Socket.on("invalidFileName", function(data) {
-		$window.alert("Invalid FileName");
-		$('#newFileName').html('<i>New Note</i>');
+		window.alert("Invalid FileName");
+		ui.clearFileName();
+		ui.focusOnNewFileName();
 	});
 	Socket.on("fileUpdated", function(data) {
 		console.log("file updated");
 		pn.load();
 	});
-
 }
 
 
-function loadExtensions(pn){
-console.log("Loading pn extensions");
-//allow to click links on the content editable
-(function (){
-$('#input a').live('click',function(event){
-	if(!pn.currentPath) return;
-	event.preventDefault();var url=$(event.target).attr("href"); window.open(url, '_blank');
-   window.focus();})
-$('#input a').live('hover',function(){$('#input').attr("contenteditable",false)},function(){
-	if(!pn.currentPath) return;
-	$('#input').attr("contenteditable",true)});
-})();
+function loadExtensions(pn) {
+	console.log("Loading pn extensions");
+	//allow to click links on the content editable
+	(function() {
+		$('#input a').live('click', function(event) {
+			if(!pn.isInitialized) return;
+			event.preventDefault();
+			var url = $(event.target).attr("href");
+			window.open(url, '_blank');
+			window.focus();
+		})
+		$('#input a').live('hover', function() {
+			$('#input').attr("contenteditable", false)
+		}, function() {
+			if(!pn.isInitialized) return;
+			$('#input').attr("contenteditable", true)
+		});
+	})();
 
 }
---
+++ public/javascripts/ui.js
@@ -0,0 +1,240 @@
+var UI = function(pn) {
+		var ui = this;
+		var pn = pn;
+
+		//CONSTANTS
+		ui.SAVING = 0;
+		ui.SAVED = 1;
+
+		var Store = {}
+
+		ui.hideInput = function() {
+			$("#input").hide();
+		}
+
+		ui.setSaveStatus = function(status) {
+			if(status == ui.SAVING) {
+				$("#saveStatus").html("Saving...");
+				return;
+			}
+			if(status == ui.SAVED) {
+				$("#saveStatus").html("Saved");
+				return;
+			}
+		}
+
+		ui.clearFileName = function() {
+			$("#newFileName").html("")
+		}
+		ui.focusOnNewFileName = function() {
+			$("#newFileName").html("")
+		}
+
+		ui.hideCommands = function() {
+			$(".command").hide();
+		}
+
+
+
+		/*
+		 *load or reload the tabs, and load
+		 */
+
+		this.initUserInterface = function(tabs){
+			console.log("init User Interface", tabs)
+			$("#inputs-navs").html("");
+			$(".command").show();
+			$("#pass").hide();
+			$("#login").hide()
+			$("#connect").hide();
+			$("#userName").html($("#login").val());
+			$('#userName').show();
+			$("#logout").show();
+			var nav;
+			var tab;
+			var empty = true;
+			for(var i = 0; i < tabs.length; i++) {
+				var nav = $('<li>').html('<a class="file" href="#" data-toggle="tab">' + decodeURIComponent(tabs[i]) + '</a>')
+				//if next path has been set on the createFile function
+				if(Store.nextPath == decodeURIComponent(tabs[i])) {
+					$(nav).addClass("active");
+					empty = false;
+				}
+				$("#inputs-navs").append(nav);
+			};
+			//Autoload 
+			if(!empty) {
+				pn.load();
+			}
+		}
+
+		$("#connect").click(function(event) {
+			event.preventDefault();
+			pn.loadUserDatas();
+			return false;
+		});
+
+		$("#login").keypress(function(event) {
+			if(!(event.which == 13)) {
+				return true;
+			} else {
+				$("#pass").focus();
+				event.preventDefault();
+				return false;
+			}
+		});
+
+		//trigger the user verification when the enter key is pressed
+		$("#pass").keypress(function(event) {
+			if(!(event.which == 13)) {
+				return true;
+			} else {
+				event.preventDefault();
+				pn.loadUserDatas();
+				return false;
+			}
+		});
+
+		// there is no cookie and no form, so reloading the page will log out
+		$("#logout").click(function() {
+			window.location = ""
+		});
+
+		//Reload the current file
+		$("#refresh").click(function(event) {
+			event.preventDefault();
+			document.getElementById("input").innerHTML = "";
+			Store.nextPath = $("#input").data("path");
+			pn.loadUserDatas();
+			return false;
+		})
+
+		/**
+		 *Load a file when the user click on a tab
+		 *The file to load is determined via the active element bootstrap put on tabs
+		 */
+		$("#inputs-navs li a.file").live('click', function(event) {
+			event.preventDefault();
+			pn.load();
+			return false;
+		})
+		/**
+		 *
+		 */
+		$('#deleteFile').live("click", function(event) {
+			event.preventDefault();
+			var fileName = $("#input").data('path');
+			if(!fileName) {
+				window.alert("Please select the file that you want to delete first");
+				return;
+			}
+			var toDelete = window.confirm("Are you sure that you want to delete the file " + decodeURIComponent(fileName));
+			if(toDelete) {
+				pn.deleteFile();
+			}
+			return false;
+		});
+
+
+		/*
+		 * variable to prevent data loss when changing file
+		 */
+
+		$("#input").focusout(function(event) {
+			event.preventDefault();
+			if(Store.inputSync) {
+				pn.save();
+			}
+		});
+
+		/**
+		 * Save button for tactile interfaces
+		 */
+		$('#save').click(function(event) {
+			event.preventDefault();
+			console.log("SAVE");
+			if(Store.inputSync) {
+				pn.save();
+				Store.inputSync = false;
+			}
+			return false;
+		});
+
+		/**
+		 * Auto save when the content is modified
+		 */
+
+		//the time the user has to stop typing for the saved to be performed
+		var TIME_OUT_VALUE = 300;
+		//the saveStatus var is used in the state machine
+		Store.currentState = "DONE_TYPING";
+		$("#input").keyup(function(e) {
+			Store.inputContent = $("#input").val();
+			if(Store.currentState == "DONE_TYPING") {
+				ui.setSaveStatus(ui.SAVING)
+				Store.currentState = "IS_TYPING";
+			};
+			if(Store.currentState == "IS_TYPING") {
+				//this variable will show weither the content has been modified after the call to
+				//setTimeout. If not it will be saved. Without this, every keyUp event will trigger a save
+				var lastInputContent = $("#input").html();
+				setTimeout(function() {
+					var inputContent = $("#input").html();
+					if(inputContent == lastInputContent && Store.lastSavedInput != inputContent) {
+						Store.currentState = "DONE_TYPING";
+						Store.lastSavedInput = inputContent;
+						pn.save();
+
+					} else {
+						return;
+					}
+				}, TIME_OUT_VALUE);
+			}
+		});
+
+
+		/*
+		 *handle ctrl+s to save the #input
+		 */
+		$("#input").live('keypress', function(event) {
+			if(!(event.which == 115 && event.ctrlKey) && !(event.which == 19)) {
+				return true;
+			} else {
+				pn.save();
+				event.preventDefault();
+				return false;
+			}
+		});
+
+		/*
+		 * Create a file when enter is pressed
+		 */
+		$("#newFileName").live('keypress', function(event) {
+			if(!(event.which == 13)) {
+				return true;
+			} else {
+				pn.createFile($('#newFileName').html());
+				event.preventDefault();
+				return false;
+			}
+		});
+		/*
+		 * Either create a new file if the #newFileName has been modified by the user,
+		 * or set the focus on the #newFile
+		 */
+		$("#addNewFile").click(function(event) {
+			event.preventDefault();
+			var fileName;
+			if(!(fileName = $('#newFileName').val())) {
+				ui.clearFileName();
+				ui.focusOnNewFileName();
+			} else {
+				pn.createFile(fileName);
+				ui.clearFileName();
+			}
+			return false;
+		});
+
+
+		return ui;
+		}
