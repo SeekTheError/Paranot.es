9b571ea0822e773958922660e1d8eabac20f1cda
Rémi Bouchar <remi.bouchar@gmail.com>
2014-07-18 11:40:25 +0200

    jshint and stict p1

--- app.js
+++ app.js
@@ -1,120 +1,75 @@
-/**
- * Module dependencies.
- */
+'use strict';
 
-var cluster = require('cluster');
-var numCPUs = require('os').cpus().length;
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var socketio = require('socket.io');
 
-// code for handling worker, one by cpu unit
-if(cluster.isMaster) {
-  var timeouts = [];
-
-  function errorMsg(worker) {
-    console.error("Something must be wrong with the connection ... " + (worker ? worker.id : ''));
-    console.log()
-
-  }
-  //when a new worker is connected
-  cluster.on('online', function(worker) {
-    console.log("new worker online: " + worker.id);
-  });
-  // when a worker is forked
-  cluster.on('fork', function(worker) {
-    timeouts[worker.id] = setTimeout(errorMsg, 2000);
-  });
-  //when the master proccess check a worker 
-  cluster.on('listening', function(worker, address) {
-    clearTimeout(timeouts[worker.id]);
-  });
-  //when a worker is terminated
-  cluster.on('exit', function(worker, code, signal) {
-    clearTimeout(timeouts[worker.id]);
-    errorMsg(worker);
-    console.log("trying to restart a worker");
-    cluster.fork();
-  });
-
-  console.log("forking " + numCPUs + "workers")
-  // Fork workers.
-  for(var i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+var app = express();
 
 
-}
-// code for the forks
-else {
-  var express = require('express'),
-    routes = require('./routes'),
-    http = require('http'),
-    path = require('path'),
-    socketio = require('socket.io');
+var port = process.env.PORT;
+app.configure(function() {
+  app.set('port', port || 3000);
+ // app.set('port', port || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.logger("short"));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
 
-  var app = express();
+});
 
+app.configure('development', function() {
+  app.use(express.errorHandler());
+});
 
-var port = process.argv[2];
-  app.configure(function() {
-    app.set('port', port || 3000);
-   // app.set('port', port || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.use(express.favicon());
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.logger("short"));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-
-  });
-
-  app.configure('development', function() {
-    app.use(express.errorHandler());
-  });
-
-  app.get('/', routes.index);
-  app.post('/createUser', routes.createUser);
-  app.post('/checkUser', routes.checkUser);
-  app.post('/deleteFile', routes.deleteFile);
+app.get('/', routes.index);
+app.post('/createUser', routes.createUser);
+app.post('/checkUser', routes.checkUser);
+app.post('/deleteFile', routes.deleteFile);
 
 
-  var server = http.createServer(app).listen(app.get('port'), function() {
-    console.log("Express server listening on port " + app.get('port'));
-  });
+var server = http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
 
-  io = socketio.listen(server);
+io = socketio.listen(server);
 
-  var redisPort = 6379;
-  var redisHost = 'localhost';
+var redisPort = 6379;
+var redisHost = 'localhost';
 
-  var RedisStore = require('socket.io/lib/stores/redis'),
-    redis = require('socket.io/node_modules/redis'),
-    pub = redis.createClient(redisPort, redisHost),
-    sub = redis.createClient(redisPort, redisHost),
-    store = redis.createClient(redisPort, redisHost);
+var RedisStore = require('socket.io/lib/stores/redis'),
+  redis = require('socket.io/node_modules/redis'),
+  pub = redis.createClient(redisPort, redisHost),
+  sub = redis.createClient(redisPort, redisHost),
+  store = redis.createClient(redisPort, redisHost);
 
-  //global redis client
-  client = redis.createClient(redisPort, redisHost);
+//global redis client
+client = redis.createClient(redisPort, redisHost);
 
-  //no auth for now;
-  /*pub.auth(password, function (err) { if (err) throw err; });
+//no auth for now;
+/*pub.auth(password, function (err) { if (err) throw err; });
 sub.auth(password, function (err) { if (err) throw err; });
 client.auth(password, function (err) { if (err) throw err; });*/
 
-  io.configure(function() {
-    io.enable('browser client minification'); // send minified client
-    io.enable('browser client etag'); // apply etag caching logic based on version number
-    io.enable('browser client gzip'); // gzip the file
-    io.set('log level', 1); // reduce logging
-    io.set('transports', [ // enable all transports (optional if you want flashsocket)
-    'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']);
-    var RedisStore = require('socket.io/lib/stores/redis');
-    io.set('store', new RedisStore({
-      redisPub: pub,
-      redisSub: sub,
-      redisClient: store
-    }));
-  });
+io.configure(function() {
+  io.enable('browser client minification'); // send minified client
+  io.enable('browser client etag'); // apply etag caching logic based on version number
+  io.enable('browser client gzip'); // gzip the file
+  io.set('log level', 1); // reduce logging
+  io.set('transports', [ // enable all transports (optional if you want flashsocket)
+  'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']);
+  var RedisStore = require('socket.io/lib/stores/redis');
+  io.set('store', new RedisStore({
+    redisPub: pub,
+    redisSub: sub,
+    redisClient: store
+  }));
+});
 
-  var realtime = require('./realtime/rt.js');
-}
+var realtime = require('./realtime/rt.js');
